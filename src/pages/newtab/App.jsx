c721719d6abad3a8b2e7ef3acc7e5effd17275ea
BlockHaity <1216692740@qq.com>
2026-04-18 23:06:33 +0800

    feat: add toggleable GitHub component and update layout

--- src/pages/newtab/App.jsx
+++ src/pages/newtab/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './App.css'
 
 // init material theme
@@ -11,15 +12,21 @@ import Search from '../../components/search'
 import Card from '../../components/Card'
 import Time from '../../components/time'
 import Icon from '../../components/Icon'
+import Github from './functions/github'
 
 // WMC
 import '@material/web/button/filled-tonal-button'
+import '@material/web/switch/switch.js'
 
 function App() {
+    const [showGithub, setShowGithub] = useState(false);
+
     return (
         <>
-            <Search />
-            <div className="container">
+            <div className="top">
+                <Search />
+            </div>
+            <div className="content">
                 <div className="left">
                     <Card>
                         <h3>Hi,BlockHaity</h3>
@@ -35,8 +42,21 @@ function App() {
                             <md-filled-tonal-button>DeepSeek</md-filled-tonal-button>
                         </div>
                     </Card>
+                    <Card>
+                        <h3>卡片控制</h3>
+                        <label style={{ display: 'flex', alignItems: 'center',gap: '10px', cursor: 'pointer'}}>
+                            <input 
+                                type="checkbox" 
+                                checked={showGithub} 
+                                onChange={(e) => setShowGithub(e.target.checked)}
+                            />
+                            GitHub
+                        </label>
+                    </Card>
                 </div>
-                <div className="right"></div>
+                <div className="right">
+                    {showGithub && <Card><Github /></Card>}
+                </div>
             </div>
         </>
     )
